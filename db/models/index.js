dcfc673ba00e963d08db7247d9e4118728be530c
Linh Huynh <linh.huynh@sjsu.edu>
2021-07-08 13:48:02 -0700

    second-commit
    fixing homepage

--- db/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    Camp: require("./Camp"),
-    User: require("./User")
-  };
-
-  
--- util/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export default {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-    GET_ERRORS : 'GET_ERRORS',
-}
